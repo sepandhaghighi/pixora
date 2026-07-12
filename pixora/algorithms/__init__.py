618674a1299651ea0a7b570895ba5f26e3663f04
sepandhaghighi <sepand.haghighi@yahoo.com>
2026-07-12 17:15:39 +0330

    feat : add algorithms module

---
+++ pixora/algorithms/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""pixora algorithms."""
+
+from .base import Algorithm
+from .nearest_neighbor import NearestNeighbor
+
+__all__ = [
+    "Algorithm",
+    "NearestNeighbor",
+]
---
+++ pixora/algorithms/base.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""pixora base algorithm."""
+
+from __future__ import annotations
+from abc import ABC, abstractmethod
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from PIL.Image import Image as PILImage
+
+
+class Algorithm(ABC):
+    """
+    Base class for all Pixora algorithms.
+    """
+
+    @abstractmethod
+    def apply(self, image: PILImage) -> PILImage:
+        """
+        Apply the algorithm.
+
+        :param image: input image
+        """
