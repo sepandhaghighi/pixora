16c5066915182f981839ee85d9e9aa01cb9e3dee
sepandhaghighi <sepand.haghighi@yahoo.com>
2026-07-19 13:13:45 +0330

    fix : update tests

--- tests/test_algorithms.py
+++ tests/test_algorithms.py
@@ -3,7 +3,7 @@
 from PIL import Image
 import pytest
 
-from pixora import NearestNeighbor, Lanczos
+from pixora import NearestNeighbor, Lanczos, Bilinear
 
 
 @pytest.mark.parametrize(
@@ -11,14 +11,10 @@ from pixora import NearestNeighbor, Lanczos
     [
         NearestNeighbor(pixel_size=8),
         Lanczos(pixel_size=8),
+        Bilinear(pixel_size=8)
     ],
 )
 def test_algorithms_apply_returns_new_image(algorithm):
-    """
-    Test algorithms return a new Pillow image.
-
-    :param algorithm: pixelization algorithm
-    """
     image = Image.new("RGB", (64, 64), "red")
 
     result = algorithm.apply(image)
@@ -38,14 +34,12 @@ def test_algorithms_apply_returns_new_image(algorithm):
         Lanczos(pixel_size=1),
         Lanczos(pixel_size=8),
         Lanczos(pixel_size=32),
+        Bilinear(pixel_size=1),
+        Bilinear(pixel_size=8),
+        Bilinear(pixel_size=32),
     ],
 )
 def test_algorithms_preserve_image_size(algorithm):
-    """
-    Test algorithms preserve image size.
-
-    :param algorithm: pixelization algorithm
-    """
     image = Image.new("RGB", (100, 80), "blue")
 
     result = algorithm.apply(image)
@@ -60,13 +54,9 @@ def test_algorithms_preserve_image_size(algorithm):
         (NearestNeighbor(pixel_size=16), 16),
         (Lanczos(pixel_size=4), 4),
         (Lanczos(pixel_size=16), 16),
+        (Bilinear(pixel_size=4), 4),
+        (Bilinear(pixel_size=16), 16),
     ],
 )
 def test_algorithms_store_pixel_size(algorithm, pixel_size):
-    """
-    Test algorithms store pixel size.
-
-    :param algorithm: pixelization algorithm
-    :param pixel_size: expected pixel size
-    """
     assert algorithm._pixel_size == pixel_size
