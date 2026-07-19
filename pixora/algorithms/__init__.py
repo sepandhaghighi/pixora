97c90e7d1432eb9934df70061e17c726b376f9e2
sepandhaghighi <sepand.haghighi@yahoo.com>
2026-07-19 13:11:51 +0330

    fix : update CLI

--- pixora/algorithms/__init__.py
+++ pixora/algorithms/__init__.py
@@ -4,9 +4,11 @@
 from .base import Algorithm
 from .nearest_neighbor import NearestNeighbor
 from .lanczos import Lanczos
+from .bilinear import Bilinear
 
 __all__ = [
     "Algorithm",
     "NearestNeighbor",
-    "Lanczos"
+    "Lanczos",
+    "Bilinear"
 ]
