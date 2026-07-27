0adeec45622c7b294c7def6daeceb3c1e7a37499
sepandhaghighi <sepand.haghighi@yahoo.com>
2026-07-27 09:20:42 +0330

    fix : remove unused import

--- pixora/cli.py
+++ pixora/cli.py
@@ -3,7 +3,6 @@
 from __future__ import annotations
 from typing import Optional
 import argparse
-import sys
 from art import tprint
 from .algorithms import NearestNeighbor, Lanczos, Bilinear, Bicubic
 from .params import DEFAULT_PIXEL_SIZE, EXIT_MESSAGE
